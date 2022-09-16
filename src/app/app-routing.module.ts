f287bac36fa4013c4124dda845c997d745ae8a08
Moro-max <mkouaelvis@gmail.com>
2022-09-16 16:10:11 +0000

    mise a jour

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'eshop', component: EshopComponent },
+  { path: 'compte', component: MonCompteComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarNavComponent } from './components/bar-nav/bar-nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AsideComponent } from './components/aside/aside.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarNavComponent,
+    HomeComponent,
+    GameComponent,
+    NotFoundPageComponent,
+    FooterComponent,
+    MonCompteComponent,
+    EshopComponent,
+    ProductComponent,
+    CartComponent,
+    AsideComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
